b959b636d514ac3c3a84d65f8e6f4fbc50b13577
Web__Dev <murod.london@gmail.com>
2022-06-14 13:36:31 +0100

    Annalyn's Infiltration (Logical Operators)

--- main.js
+++ main.js
@@ -1,71 +1,36 @@
-// Get an Item
-const numbers = [1, 2, 3, 4, 5];
-
-const getItem = (cards, position) => {
-  return (cards[position] = 1);
+const canFreePrison = () => {
+  const knightIsAwake = false;
+  const archerIsAwake = true;
+  const prisonerIsAwake = false;
+  const petDogIsPresent = false;
+  console.log(
+    (!knightIsAwake &&
+      !archerIsAwake &&
+      prisonerIsAwake) ||
+      (petDogIsPresent && !archerIsAwake),
+  );
 };
-console.log(getItem(numbers));
+canFreePrison();
 
-// Replace index of item
-const amount = [1, 2, 4, 1];
-
-function setItem(cards, position = 2, replaceMent = 6) {
-  // Default Parameter
-  cards[position] = replaceMent;
-  return cards;
-}
-console.log(setItem(amount));
-
-// Add an item end of array
-const numeric = [1, 2, 3, 4];
-
-const addNumber = (cards, position = 8) => {
-  // Default Parameter
-  cards.push(position);
-  return cards;
+const canSignalPrisoner = () => {
+  const archerIsAwake = false;
+  const prisonerIsAwake = true;
+  console.log(!archerIsAwake && prisonerIsAwake);
 };
-console.log(addNumber(numeric));
+canSignalPrisoner();
 
-// Remove an element from index
-const counts = [1, 2, 3, 4, 5];
-
-const removeItem = (cards, position) => {
-  cards.splice(position, 1);
-  return cards;
+const canSpy = () => {
+  const knightIsAwake = false;
+  const archerIsAwake = true;
+  const prisonerIsAwake = false;
+  console.log(
+    knightIsAwake || archerIsAwake || prisonerIsAwake, // Short circutting
+  );
 };
-console.log(removeItem(counts));
+canSpy();
 
-// Remove an element from end of Array
-const dates = [1, 2, 3, 4];
-
-const removeFromEnd = (cards) => {
-  cards.pop();
-  return cards;
+const fastAttack = () => {
+  const knightIsAwake = true;
+  console.log(!knightIsAwake);
 };
-console.log(removeFromEnd(dates));
-
-// Add an element from top of Array
-const types = [1, 2, 3, 4];
-
-const addFromTop = (cards, newCard = 'str') => {
-  cards.unshift(newCard);
-  return cards;
-};
-console.log(addFromTop(types));
-
-// Remove an element from top of Array
-const sum = [1, 2, 3, 4];
-
-const removeFromTop = (cards) => {
-  cards.shift();
-  return cards;
-};
-console.log(removeFromTop(sum));
-
-// Check size of Array
-const totalNumbers = [1, 2, 3, 4, 5];
-
-const checkSizeOfArray = (cards, stackSize) => {
-  return cards === stackSize ? true : false;
-};
-console.log(checkSizeOfArray(totalNumbers));
+fastAttack();
