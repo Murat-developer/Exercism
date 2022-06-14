2abf67216389f557b6a7b6b5f7cd14aa1fe10c3a
Web__Dev <murod.london@gmail.com>
2022-06-14 17:43:05 +0100

    Poetry Club Door Policy (String, index...)

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const canFreePrison = () => {
-  const knightIsAwake = false;
-  const archerIsAwake = true;
-  const prisonerIsAwake = false;
-  const petDogIsPresent = false;
-  console.log(
-    (!knightIsAwake &&
-      !archerIsAwake &&
-      prisonerIsAwake) ||
-      (petDogIsPresent && !archerIsAwake),
+// Get the first letter
+const string =
+  'Stands so high Huge hooves too Impatiently!';
+
+const getFirstLetter = (input) => {
+  return input.charAt(0);
+};
+console.log(getFirstLetter(string));
+
+// Capitalize a word
+const word = 'SHiRE'; // shire result we be same
+
+const capitalWord = (str) => {
+  return (
+    str.charAt(0).toUpperCase() +
+    str.slice(1).toLowerCase()
   );
 };
-canFreePrison();
+console.log(capitalWord(word));
 
-const canSignalPrisoner = () => {
-  const archerIsAwake = false;
-  const prisonerIsAwake = true;
-  console.log(!archerIsAwake && prisonerIsAwake);
-};
-canSignalPrisoner();
+// Get last letter of sentece
+const letter = 'Stands so high';
 
-const canSpy = () => {
-  const knightIsAwake = false;
-  const archerIsAwake = true;
-  const prisonerIsAwake = false;
-  console.log(
-    knightIsAwake || archerIsAwake || prisonerIsAwake, // Short circutting
-  );
+const getLastLetter = (letter) => {
+  const cut = letter.trim();
+  return cut[cut.length - 1];
 };
-canSpy();
+console.log(getLastLetter(letter));
 
-const fastAttack = () => {
-  const knightIsAwake = true;
-  console.log(!knightIsAwake);
+// Be polite
+const polite = 'horse';
+
+const bePolite = (input) => {
+  const start = input.charAt(0).toUpperCase();
+  const end = input.slice(1).toLowerCase();
+  return `${start}${end}, please `; // Template Littler
 };
-fastAttack();
+console.log(bePolite(polite));
