b7ef236f3d8b6d0f5379f10b22781f67da9b105f
Web__Dev <murod.london@gmail.com>
2022-06-14 20:05:03 +0100

    Elyses Enchantments (Arrays practice)

--- main.js
+++ main.js
@@ -1,38 +1,73 @@
-// Get the first letter
-const string =
-  'Stands so high Huge hooves too Impatiently!';
-
-const getFirstLetter = (input) => {
-  return input.charAt(0);
+// Replace positioin
+const numbers = [1, 2, 4, 1];
+const replacePosition = (cards, position = 2) => {
+  return cards[position];
 };
-console.log(getFirstLetter(string));
+console.log(replacePosition(numbers));
 
-// Capitalize a word
-const word = 'SHiRE'; // shire result we be same
+// Exchange a card in the stack
+const numerics = [1, 2, 4, 1];
 
-const capitalWord = (str) => {
-  return (
-    str.charAt(0).toUpperCase() +
-    str.slice(1).toLowerCase()
-  );
+const setItem = (
+  cards,
+  position = 2,
+  replacementCard = 6,
+) => {
+  cards[position] = replacementCard;
+  return cards;
 };
-console.log(capitalWord(word));
+console.log(setItem(numerics));
 
-// Get last letter of sentece
-const letter = 'Stands so high';
+// Insert a card at the top of the stack
+const cards = [5, 9, 7, 1];
 
-const getLastLetter = (letter) => {
-  const cut = letter.trim();
-  return cut[cut.length - 1];
+const insertItemAtTop = (cards, newCard = 8) => {
+  cards.push(newCard);
+  return cards;
 };
-console.log(getLastLetter(letter));
+console.log(insertItemAtTop(cards));
 
-// Be polite
-const polite = 'horse';
+// Remove a card from the stack
+const cardItems = [3, 2, 6, 4, 8];
 
-const bePolite = (input) => {
-  const start = input.charAt(0).toUpperCase();
-  const end = input.slice(1).toLowerCase();
-  return `${start}${end}, please `; // Template Littler
+const removeCard = (cards, position = 2) => {
+  cards.splice(position, 1);
+  return cards;
 };
-console.log(bePolite(polite));
+console.log(removeCard(cardItems));
+
+// Remove the top card from the stack
+const numCards = [3, 2, 6, 4, 8];
+
+const removeFromTop = (cards) => {
+  cards.pop();
+  return cards;
+};
+console.log(removeFromTop(numCards));
+
+// Insert a card at the bottom of lthe stack
+const cardsNum = [5, 9, 7, 1];
+
+const insertCard = (cards) => {
+  cards.unshift(8);
+  return cards;
+};
+console.log(insertCard(cardsNum));
+
+// Remove a card from the bottom
+const numericCard = [8, 5, 9, 7, 1, 3];
+
+const removeFromBottomCard = (cards) => {
+  cards.shift();
+  return cards;
+};
+console.log(removeFromBottomCard(numericCard));
+
+// Check the size of the stack
+
+const stackNumbers = [3, 2, 6, 4, 8];
+
+const checkSizeOfStack = (stackes) => {
+  return stackes.length === 4;
+};
+console.log(checkSizeOfStack(stackNumbers));
