82f07f52ec28c283fd6c013ceed32d1b5d44220f
Web__Dev <murod.london@gmail.com>
2022-06-17 14:53:30 +0100

    Raindrops

--- main.js
+++ main.js
@@ -1,73 +1,20 @@
-// Replace positioin
-const numbers = [1, 2, 4, 1];
-const replacePosition = (cards, position = 2) => {
-  return cards[position];
+// Raindrops
+
+const convert = (rain) => {
+  let output = '';
+  if (rain % 3 === 0 && rain % 5 === 0 && rain % 7 === 0)
+    return 'PlingPlangPlong';
+  if (rain % 3 === 0 && rain % 5 === 0)
+    return 'PlingPlang';
+  if (rain % 3 === 0 && rain % 7 === 0)
+    return 'PlingPlong';
+  if (rain % 5 === 0 && rain % 7 === 0)
+    return 'PlangPlong';
+  if (rain % 3 === 0) return 'Pling';
+  if (rain % 5 === 0) return 'Plang';
+  if (rain % 7 === 0) return 'Plong';
+  if (rain % 3 !== 0 && rain % 5 !== 0 && rain % 7 !== 0)
+    output += rain;
+  return output;
 };
-console.log(replacePosition(numbers));
-
-// Exchange a card in the stack
-const numerics = [1, 2, 4, 1];
-
-const setItem = (
-  cards,
-  position = 2,
-  replacementCard = 6,
-) => {
-  cards[position] = replacementCard;
-  return cards;
-};
-console.log(setItem(numerics));
-
-// Insert a card at the top of the stack
-const cards = [5, 9, 7, 1];
-
-const insertItemAtTop = (cards, newCard = 8) => {
-  cards.push(newCard);
-  return cards;
-};
-console.log(insertItemAtTop(cards));
-
-// Remove a card from the stack
-const cardItems = [3, 2, 6, 4, 8];
-
-const removeCard = (cards, position = 2) => {
-  cards.splice(position, 1);
-  return cards;
-};
-console.log(removeCard(cardItems));
-
-// Remove the top card from the stack
-const numCards = [3, 2, 6, 4, 8];
-
-const removeFromTop = (cards) => {
-  cards.pop();
-  return cards;
-};
-console.log(removeFromTop(numCards));
-
-// Insert a card at the bottom of lthe stack
-const cardsNum = [5, 9, 7, 1];
-
-const insertCard = (cards) => {
-  cards.unshift(8);
-  return cards;
-};
-console.log(insertCard(cardsNum));
-
-// Remove a card from the bottom
-const numericCard = [8, 5, 9, 7, 1, 3];
-
-const removeFromBottomCard = (cards) => {
-  cards.shift();
-  return cards;
-};
-console.log(removeFromBottomCard(numericCard));
-
-// Check the size of the stack
-
-const stackNumbers = [3, 2, 6, 4, 8];
-
-const checkSizeOfStack = (stackes) => {
-  return stackes.length === 4;
-};
-console.log(checkSizeOfStack(stackNumbers));
+console.log(convert(105));
