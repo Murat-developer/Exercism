39e7cf780e165323e2e5579d51e71a152c9457ce
Web__Dev <murod.london@gmail.com>
2022-06-19 08:01:59 +0100

    Reverse String.

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-const numbers = [1, 2, 3, 4];
+const string = 'John Smith';
 
-const includes = (array, searchElement) => {
-  // for (let value of array)
-  //   if (value === searchElement) return true;
-
-  // return false;
-
-  // return array.filter((value) => {
-  //   return value === searchElement;
-  // });
-
-  return array.some((item) => {
-    return item === searchElement;
-  });
+const reverseStr = (input) => {
+  return input.split('').reverse().join('');
 };
-
-console.log(includes(numbers, 2));
+console.log(reverseStr(string));
