7dfb9f1449c700d07d657933b26370e4c2aa3214
Web__Dev <murod.london@gmail.com>
2022-06-26 15:53:24 +0100

    Calculate number of workdays

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-// Calculate day rate given an hourly rate;
+// Calculate number of workdays
 
-const dayRate = (ratePerHour = 89) => {
-  // Default pramader
-  const hoursDay = 8;
-  return ratePerHour * hoursDay;
+const daysInBudget = (
+  budget = 20000,
+  ratePerHour = 89,
+) => {
+  const fixedDays = 8;
+  const dayRate = ratePerHour * fixedDays;
+
+  return Math.floor(budget / dayRate);
 };
-console.log(dayRate());
+console.log(daysInBudget());
