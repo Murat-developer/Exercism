08e48fe9c37bf01847a970205715d7babb8d32c9
Web__Dev <murod.london@gmail.com>
2022-06-26 17:28:32 +0100

    Second iteration Get first letter of sentence

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let str =
   'Stands so high huge hooves too Impatiently wait for it';
 
 const frontDoorResponse = (line) => {
-  return line.charAt(0);
+  // return line.charAt(0);
+  return line[0];
 };
 console.log(frontDoorResponse(str));
