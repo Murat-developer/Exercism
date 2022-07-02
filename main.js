e3bacde2681ddf8c44b3d66da04f99e5f0bbf884
Web__Dev <murod.london@gmail.com>
2022-07-02 21:55:38 +0100

    String High and low 2 iteration

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const highAndLow = (numbers) => {
   numbers = numbers.split(' ');
-  return (
-    Math.max(...numbers) + ' ' + Math.min(...numbers)
-  );
+  return `${Math.max(...numbers)} ${Math.min(
+    ...numbers,
+  )}`;
 };
 console.log(highAndLow('1 2 3'));
