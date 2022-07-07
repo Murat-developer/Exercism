c364629c31c86f6133dee9124eb3fcf6609a46f7
Web__Dev <murod.london@gmail.com>
2022-07-07 20:31:13 +0100

    Count Years

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const sentenceSmash = (word) => {
-  return word.join(' ');
+const countYears = (p, i, t, d) => {
+  let years = 0;
+  while (p < d) {
+    p += p * i * (1 - t);
+  }
+  return years;
 };
-console.log(
-  sentenceSmash([
-    'Hello',
-    'World',
-    'this',
-    'is',
-    'great',
-  ]),
-);
+
+console.log(countYears(1000, 0.05, 0.18, 1100));
