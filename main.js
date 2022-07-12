0fbcab2e87e98a03750db5d0e55dc45a10645c96
Web__Dev <murod.london@gmail.com>
2022-07-12 21:14:31 +0100

    Cheessboard

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const countYears = (p, i, t, d) => {
-  let years = 0;
-  while (p < d) {
-    p += p * i * (1 - t);
-  }
-  return years;
-};
+// var makeGrid = function (l, w) {
+//   var odd = ' #'.repeat(w);
+//   var even = '# '.repeat(w);
 
-console.log(countYears(1000, 0.05, 0.18, 1100));
+//   for (i = 1; i <= l; i++) {
+//     if (i % 2 == 0 || i == 2) {
+//       console.log(even);
+//     } else {
+//       console.log(odd);
+//     }
+//   }
+// };
+// makeGrid(4, 4);
+
+const cheessboard = (input) => {
+  let output = '';
+  for (let i = 0; i < input; i++) {
+    for (let x = 0; x < input; x++) {
+      if ((x + i) % 2 === 0) output = output += ' ';
+      else output = output += '#';
+    }
+    output += '\n';
+  }
+  console.log(output);
+};
+cheessboard(8);
