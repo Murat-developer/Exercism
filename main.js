350ed79be12cfbca579d4d10e2ebb5aea879640f
Web__Dev <murod.london@gmail.com>
2022-07-13 21:42:06 +0100

    Cheessboard 2 iteration

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-// var makeGrid = function (l, w) {
-//   var odd = ' #'.repeat(w);
-//   var even = '# '.repeat(w);
-
-//   for (i = 1; i <= l; i++) {
-//     if (i % 2 == 0 || i == 2) {
-//       console.log(even);
-//     } else {
-//       console.log(odd);
-//     }
-//   }
-// };
-// makeGrid(4, 4);
-
 const cheessboard = (input) => {
   let output = '';
-  for (let i = 0; i < input; i++) {
-    for (let x = 0; x < input; x++) {
-      if ((x + i) % 2 === 0) output = output += ' ';
-      else output = output += '#';
+  for (let i = 1; i <= input; i++) {
+    for (let x = 1; x <= input; x++) {
+      (i + x) % 2 === 0
+        ? (output += ' ')
+        : (output += '#');
     }
     output += '\n';
   }
